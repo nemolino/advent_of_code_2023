ad6062ef11c8e9a5ad636a6b8240c29c77de6910
Alessandro Minoli <minoli.alessandro@gmail.com>
2023-12-19 23:48:27 +0100

    upload day 10

--- 10/sol.py
+++ 10/sol.py
@@ -1,31 +1,36 @@
+# idea for part 2 : look at the maths explained in resource_01.png and resource_02.png in day 18
+
 with open('10/input.txt') as f:
     M = list(map(lambda x: list(x.strip()), f.readlines()))
 
-n = len(M) 
-
 # find S position
-for i in range(n):
+for i in range(len(M)):
     flag = False
-    for j in range(n):
+    for j in range(len(M[0])):
         if M[i][j] == 'S':
             s_pos = [i,j]
             flag = True
             break
     if flag: break
 
-# guardando l'input si vede che da S si può andare a dx o sotto
+# looking at input.txt we can see that from S you can go either right or down
 
 #   LJL
 #   7S-
 #   J|L     
 
+loop_tiles = [tuple(s_pos)]  # useful for part2
+
 prev_pos = s_pos[:]
-cur_pos = [s_pos[0], s_pos[1]+1]    # partiamo andando a dx
+cur_pos = [s_pos[0], s_pos[1]+1]    # we start going right
 distance = 1
+
 
 while cur_pos != s_pos:
     
     x = cur_pos[:]
+
+    loop_tiles.append(tuple(x))
 
     match M[cur_pos[0]][cur_pos[1]]:
 
@@ -38,9 +43,9 @@ while cur_pos != s_pos:
             else:                           cur_pos[1] -= 1
     
         case "L": 
-            if prev_pos[0] == cur_pos[0]-1: cur_pos[1] += 1
-            else:                           cur_pos[0] -= 1
-
+            if prev_pos[0] == cur_pos[0]-1: cur_pos[1] += 1             
+            else:                           cur_pos[0] -= 1             
+                                                                        
         case "J": 
             if prev_pos[0] == cur_pos[0]-1: cur_pos[1] -= 1
             else:                           cur_pos[0] -= 1
@@ -59,3 +64,15 @@ while cur_pos != s_pos:
     distance += 1
 
 print('part1 :', distance//2)
+
+V = []
+for t in loop_tiles:
+    if M[t[0]][t[1]] in ['L','F','J','7','S']:
+        V.append(t)
+
+n = len(V)
+A =  abs(sum((V[i][0] * V[(i+1)%n][1] - 
+              V[(i+1)%n][0] * V[i][1]) 
+              for i in range(n))) / 2
+
+print('part2 :', int(A+1-distance/2))
--- README.md
+++ README.md
@@ -6,5 +6,4 @@ My solutions to [Advent of Code](https://adventofcode.com/) 2023 problems 🌟
 
 TODO
 
-- day 10 part 2
 - day 17 part 1 and 2
